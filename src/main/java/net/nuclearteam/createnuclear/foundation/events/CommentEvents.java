6c3e27231b4d7882ecf350a6f8c0e96a68afc918
mathi <mathis.gredt@ynov.com>
2026-08-04 15:08:01 +0200

    Add iodine effect and related potions; update radiation effect handling

--- src/main/java/net/nuclearteam/createnuclear/foundation/events/CommentEvents.java
+++ src/main/java/net/nuclearteam/createnuclear/foundation/events/CommentEvents.java
@@ -1,10 +1,14 @@
 package net.nuclearteam.createnuclear.foundation.events;
 
 
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 import net.neoforged.neoforge.event.brewing.RegisterBrewingRecipesEvent;
+import net.neoforged.neoforge.event.entity.EntityAttributeModificationEvent;
+import net.nuclearteam.createnuclear.CNAttributes;
 import net.nuclearteam.createnuclear.CNPotions;
 import net.nuclearteam.createnuclear.CreateNuclear;
 import net.nuclearteam.createnuclear.content.multiblock.input.item.ReactorInputEntity;
@@ -26,4 +30,15 @@ public class CommentEvents {
         }
     }
 
+    @EventBusSubscriber(modid = CreateNuclear.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
+    public static class RadiationEvent {
+        @SubscribeEvent
+        public static void onEntityAttribute(EntityAttributeModificationEvent event) {
+            for (EntityType<? extends LivingEntity> type : event.getTypes()) {
+                event.add(type, CNAttributes.IRRADIATED_RESISTANCE);
+            }
+        }
+    }
+
 }
+
